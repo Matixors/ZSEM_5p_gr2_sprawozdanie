be983af6b762fa205e1624d6e54b348e52ffa885
Matixors <97966889+Matixors@users.noreply.github.com>
2024-10-13 22:42:54 +0200

    implemented counting sort

--- Sorting_Algo/Program.cs
+++ Sorting_Algo/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class CountingSort
+{
+    public static int[] Sort(int[] array, int minValue, int maxValue)
+    {
+        int[] countArray = new int[maxValue - minValue + 1];
+        int[] sortedArray = new int[array.Length];
+
+        foreach (var num in array)
+        {
+            countArray[num - minValue]++;
+        }
+
+        int sortedIndex = 0;
+        for (int i = 0; i < countArray.Length; i++)
+        {
+            while (countArray[i] > 0)
+            {
+                sortedArray[sortedIndex++] = i + minValue;
+                countArray[i]--;
+            }
+        }
+
+        return sortedArray;
+    }
+
+    static void Main(string[] args)
+    {
+        int[] arr = { 11, 5, 54, 23, 6};
+        var sorted = Sort(arr, arr.Min(), arr.Max());
+        Console.WriteLine("Posortwoana tablica: " + string.Join(", ", sorted));
+    }
+}
